35d3461235a32861b8725b54733794e1b470186d
ureg123 <ureg123@ujeshs-air>
2019-08-31 16:18:43 -0400

    attempt 1

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -2,13 +2,111 @@ package a1;
 
 import java.util.Scanner;
 
+
 public class A1Jedi {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+int count = scan.nextInt();
+
 		
+		String[][] nameAndPrice = new String[count][4];
+		for (int i = 0; i < count; i++) {
+			for (int j = 0; j < 2; j++) {
+				if (j == 0) {
+					nameAndPrice[i][j] = scan.next();
+				} else {
+					nameAndPrice[i][j] = String.format("%.2f", scan.nextDouble());
+				}	
+			}
+		}
+		for (int i =  0; i < count; i++) {
+			nameAndPrice[i][2] = Integer.toString(0);
+			nameAndPrice[i][3] = Integer.toString(0);
+		}
+		
+		int numberOfCustomers = scan.nextInt();
+		String[][] customerNames = new String[numberOfCustomers][3];
+		double[] pricePaid = new double[numberOfCustomers];
+		
+		
+		for (int i = 0; i < numberOfCustomers; i++) {
+			String customerFirstName = scan.next();
+			String customerLastName = scan.next();
+			customerNames[i][0] = customerFirstName;
+			customerNames[i][1] = customerLastName;
+			
+			
+			int itemsBought = scan.nextInt();
+			
+			
+			
+			String[][] quantAndNameOfItem = new String[itemsBought][2];
+			double total = 0;
+			for (int j = 0; j < itemsBought; j++) {
+				quantAndNameOfItem[j][0] = Integer.toString(scan.nextInt());
+				quantAndNameOfItem[j][1] = scan.next();
+				
+				
+				if (searchArrayBoolean(nameAndPrice, quantAndNameOfItem[j][1])) {
+					int var = Integer.parseInt(nameAndPrice[searchArrayIndex(nameAndPrice, quantAndNameOfItem[j][1])][2]);
+					var = var + 1;
+					nameAndPrice[searchArrayIndex(nameAndPrice, quantAndNameOfItem[j][1])][2] = Integer.toString(var);
+				}
+				if (searchArrayBoolean(nameAndPrice, quantAndNameOfItem[j][1])) {
+					int local = Integer.parseInt(nameAndPrice[searchArrayIndex(nameAndPrice, quantAndNameOfItem[j][1])][3]);
+					int z = Integer.parseInt(quantAndNameOfItem[j][0]);
+					local = local + z;
+					nameAndPrice[searchArrayIndex(nameAndPrice, quantAndNameOfItem[j][1])][3] = Integer.toString(local); 
+				}
+				
+				double price = A1Adept.searchArray(nameAndPrice, quantAndNameOfItem[j][1]);
+				double rev = price * Double.parseDouble(quantAndNameOfItem[j][0]);
+				total = total + rev;
+			}
+			pricePaid[i] = total;
+			 
+		}
+		for (int i = 0; i < nameAndPrice.length; i++) {
+			nameAndPrice[i][3] = " " + nameAndPrice[i][3];
+		}
+		
+		for (int i = 0; i < nameAndPrice.length; i++) {
+			System.out.println(changeToNo(nameAndPrice[i][2]) + " customers bought" + changeToNothing(nameAndPrice[i][3]) + " " + nameAndPrice[i][0]);
+		}
+		
+	}
+	
+	static int searchArrayIndex(String[][] a, String b) {
+		for (int i = 0; i < a.length; i++) {
+			if (b.equals(a[i][0])) {
+				return i;
+			}
+		}
+		return 0;
+}
+	
+	static boolean searchArrayBoolean(String[][] a, String b) {
+		for (int i = 0; i < a.length; i++) {
+			if (b.equals(a[i][0])) {
+				return true;
+			}
+		}
+		return false;
+}
+	static String changeToNothing(String a) {
+		if (a.equals(" 0")) {
+			return "";
+		}
+		return a;
+	}
+	
+	static String changeToNo(String a) {
+		if (a.equals("0")) {
+			return "No";
+		}
+		return a;
 	}
 }
